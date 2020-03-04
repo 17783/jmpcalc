cdeb47bfc75d83aa4493a180451669baf8a8d542
nullbox <nullbox@local>
2020-03-04 15:48:07 +0300

    arabic to roman parser, better result output

--- src/com/example/jmp/calc/Main.java
+++ src/com/example/jmp/calc/Main.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static final String ELEMENT_DELIMITERS = "[+\\-*/ ]+";
-    public static final String OPERATOR_DELIMITERS = "[a-z\\d\\s]+";
+    public static final String OPERATOR_DELIMITERS = "[a-zA-Z\\d\\s]+";
     public static final String ROMAN_CHECK = "[IVXivx+\\-*\\/ ]+";
 
     public static void main(String[] args) throws IOException {
@@ -21,34 +21,29 @@ public class Main {
         String[] elements = name.split(ELEMENT_DELIMITERS);
         String[] operators = name.split(OPERATOR_DELIMITERS);
         boolean roman;
-        if (name.matches(ROMAN_CHECK)) {
-           roman = true;
-            System.out.println("is roman");
-
-        } else {
-            roman = false;
-            System.out.println("is NOT roman");
-        }
-
-//        System.out.println(name);
+        roman = name.matches(ROMAN_CHECK);
+        System.out.println("using roman numerals: " + roman);
         String element0 = elements[0];
         String element1 = elements[1];
         String operator = operators[1];
-//        System.out.println(element0);
-//        System.out.println("operator is:" + operator);
-//        System.out.println(element1);
         int element0Int;
         int element1Int;
+        int result = 0;
+        String finalResult;
         if (!roman) {
             element0Int = Integer.parseInt(element0);
             element1Int = Integer.parseInt(element1);
-        } else {
-            element0Int = Parser.parseRoman(element0);
-            element1Int = Parser.parseRoman(element1);
-        }
-            int result = Computer.compute(element0Int, element1Int, operator);
-            System.out.println(name + " = " + result);
+            result = Computer.compute(element0Int, element1Int, operator);
+            finalResult = Integer.toString(result);
 
+        } else {
+            element0Int = Parser.getArabic(element0);
+            element1Int = Parser.getArabic(element1);
+            result = Computer.compute(element0Int, element1Int, operator);
+            finalResult = Parser.getRoman(result);
+        }
+
+        System.out.println(element0.toUpperCase() + " " + operator + " " + element1.toUpperCase() + " = " + finalResult);
 
 
     }
--- src/com/example/jmp/calc/Parser.java
+++ src/com/example/jmp/calc/Parser.java
@@ -1,40 +1,32 @@
 package com.example.jmp.calc;
+import java.util.Arrays;
 
 public class Parser {
-    public static int parseRoman(String element) {
-        int result = 0;
-        switch (element) {
-            case "i":
-                result = 1;
-                break;
-            case "ii":
-                result = 2;
-                break;
-            case "iii":
-                result = 3;
-                break;
-            case "iv":
-                result = 4;
-                break;
-            case "v":
-                result = 5;
-                break;
-            case "vi":
-                result = 6;
-                break;
-            case "vii":
-                result = 7;
-                break;
-            case "viii":
-                result = 8;
-                break;
-            case "ix":
-                result = 9;
-                break;
-            case "x":
-                result = 10;
+    private static int[] arabic = {1,2,3,4,5,6,7,8,9,10};
+    private static String[] roman = {"i","ii","iii","iv","v","vi","vii","viii","ix","x"};
 
-        }
+    public static int getArabic(String element) {
+        element = element.toLowerCase();
+        int index = Arrays.binarySearch(roman, element);
+        index = (index < 0) ? -1 : index;
+        int result = 0;
+        result = arabic[index];
         return result;
+    }
+
+    public static String getRoman(int number) {
+
+        String riman[] = {"M","XM","CM","D","XD","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int arab[] = {1000, 990, 900, 500, 490, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (number > 0 || arab.length == (i - 1)) {
+            while ((number - arab[i]) >= 0) {
+                number -= arab[i];
+                result.append(riman[i]);
+            }
+            i++;
+        }
+        return result.toString();
     }
 }
